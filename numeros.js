6be2670570f1a8d404dbde3801d256fcaa83d1e9
GabrielPaez <darkdelll@gmail.com>
2023-03-07 18:50:25 -0300

    segunda entrega

--- numeros.js
+++
@@ -1,9 +0,0 @@
-//numeros aleatorios del 1 al 20
-// const random = require('./numeros');
-// console.log(random.randomNumber(1,20));
-
-
-
-for (let i = 0; i < 10000 ; i++) {
-    console.log(Math.floor(Math.random() * (20) + 1));
-}
